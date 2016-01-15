8031a75093eb07d9dd85d5bb741dadf701b987a0
Raphaël RIGNIER <r.rignier@leschartreux.net>
2016-01-15 20:03:50 +0100

    Profs anonymes

---
+++ cegiddbselect/cegiddbselect/TeachersDef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cegiddbselect
+{
+    public partial class frmDbSelect
+    {
+        //Définition des professeurs par un dictionnaire indice/valeur.
+        private Dictionary<int, String> dTeachers = new Dictionary<int, string>{
+                                                    {1,"PROF 1"},
+                                                    {2,"PROF 2"},
+                                                    {3,"PROF 3"},
+                                                    {4,"PROF 4"},
+                                                    {5,"PROF 5"},
+                                                    {6,"PROF 6"},
+                                                    {99,"AUTRES"}
+                                                };
+
+    }
+}
